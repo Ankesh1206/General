3627639908167c38008116191b20e6d9f431397c
Paryanshu Sourav <paryanshu.sourav@ggktech.com>
2020-02-20 23:33:28 +0530

    My first commit

---
+++ src/test/java/PriceComparison.java
@@ -0,0 +1,43 @@
+import ApplicationUtilities.ApplicationLibrary;
+import PublicUtilities.Constant;
+import PublicUtilities.PublicLibrary;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class PriceComparison {
+    private ApplicationLibrary applicationLibrary;
+    private PublicLibrary publicLibrary;
+
+
+    /**
+     * constructor
+     */
+    public PriceComparison()
+    {
+        applicationLibrary=new ApplicationLibrary();
+        publicLibrary=new PublicLibrary();
+    }
+
+
+    @BeforeTest
+    public void beforeTest()
+    {
+        publicLibrary.setUp(Constant.chromeBrowser);
+        publicLibrary.maximizeBrowser();
+    }
+
+
+    @Test
+    public void priceComparisonTest()
+    {
+        applicationLibrary.priceComparator();
+    }
+
+
+    @AfterTest
+    public void afterTest()
+    {
+        publicLibrary.quitBrowser();
+    }
+}
